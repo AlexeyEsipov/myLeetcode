0e61bb20860d027d044e3496ce096032656a304c
Esipov Aleksey <a_esipov@list.ru>
2023-11-06 18:51:20 +0300

    Добавил бейджики

--- src/test/java/easy/BestTimeToBuyAndSellStockTest.java
+++ src/test/java/easy/BestTimeToBuyAndSellStockTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-@Disabled
+
 class BestTimeToBuyAndSellStockTest {
     @Test
     void when() {
--- src/test/java/easy/BinarySearchTest.java
+++ src/test/java/easy/BinarySearchTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-@Disabled
+
 class BinarySearchTest {
 
     @Test
